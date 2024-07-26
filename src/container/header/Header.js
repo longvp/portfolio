84faca90b06a17ed8a805e1622e1592ae943ebb7
longvp <longpv@omegatheme.com>
2024-07-26 08:34:40 +0700

    update

--- src/container/header/Header.js
+++ src/container/header/Header.js
@@ -1,77 +1,138 @@
-import React, { useEffect, useRef, useState } from 'react'
-import './Header.scss'
+import React, { useEffect, useRef, useState } from 'react';
+import './Header.scss';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAlignCenter } from '@fortawesome/free-solid-svg-icons'
-import { useStore } from '../../store/useStore'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAlignCenter } from '@fortawesome/free-solid-svg-icons';
+import { useStore } from '../../store/useStore';
 
 function Header() {
-    const headerRef = useRef(null)
-    const [showMenu, setShowMenu] = useState(false)
-    const [headerFixed, setHeaderFixed] = useState(false)
+	const headerRef = useRef(null);
+	const [showMenu, setShowMenu] = useState(false);
+	const [headerFixed, setHeaderFixed] = useState(false);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            setHeaderFixed(window.scrollY >= headerRef.current.offsetHeight)
-        }
-        window.addEventListener('scroll', handleScroll)
-        return () => {
-            window.removeEventListener('scroll', handleScroll)
-        }
-    }, [])
+	useEffect(() => {
+		const handleScroll = () => {
+			setHeaderFixed(window.scrollY >= headerRef.current.offsetHeight);
+		};
+		window.addEventListener('scroll', handleScroll);
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
 
-    const [state, dispatch] = useStore()
-    const { darkMode } = state
+	const [state, dispatch] = useStore();
+	const { darkMode } = state;
 
-    const handleToggle = () => {
-        dispatch({ type: 'toggle' })
-    }
+	const handleToggle = () => {
+		dispatch({ type: 'toggle' });
+	};
 
-    return (
-        <>
-            <div className={headerFixed === true ? 'header header-fixed' : 'header'} ref={headerRef}
-                style={{ background: darkMode ? '#051418' : '' }}
-            >
-                <div className='container header-container'>
-                    <div className='header-left'>
-                        <div className='header-name'>Pham Viet Long</div>
-                    </div>
-                    <div className='header-right'>
-                        <ul className={showMenu === true ? 'header-menu-list' : 'header-menu-list hide-menu'}
-                            style={{ background: darkMode ? '#051418' : '' }}>
-                            <li className='header-item'>
-                                <a href='#home' className='header-link' onClick={() => setShowMenu(false)} style={{ color: darkMode ? 'white' : '' }}>Home</a>
-                            </li>
-                            <li className='header-item'>
-                                <a href='#about' className='header-link' onClick={() => setShowMenu(false)} style={{ color: darkMode ? 'white' : '' }}>About</a>
-                            </li>
-                            <li className='header-item'>
-                                <a href='#skills' className='header-link' onClick={() => setShowMenu(false)} style={{ color: darkMode ? 'white' : '' }}>Skills</a>
-                            </li>
-                            <li className='header-item'>
-                                <a href='#projects' className='header-link' onClick={() => setShowMenu(false)} style={{ color: darkMode ? 'white' : '' }}>Projects</a>
-                            </li>
-                            <li className='header-item'>
-                                <a href='#contact' className='header-link' onClick={() => setShowMenu(false)} style={{ color: darkMode ? 'white' : '' }}>Contact</a>
-                            </li>
-                            <li className='btn-close-menu' onClick={() => setShowMenu(false)} style={{ color: darkMode ? 'white' : '' }}>&times;</li>
-                        </ul>
-                        <div className='btn-show-menu' onClick={() => setShowMenu(true)}>
-                            <FontAwesomeIcon icon={faAlignCenter} className='icon-show-menu' style={{ color: darkMode ? 'white' : '' }} />
-                        </div>
-                        <div className='toggle'>
-                            <input type="checkbox" id="switch" className="switch-input" onClick={() => handleToggle()} />
-                            <label htmlFor="switch" className="switch" style={{ background: darkMode ? '#37b548' : '#102a42' }}></label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/*
+	return (
+		<>
+			<div
+				className={headerFixed === true ? 'header header-fixed' : 'header'}
+				ref={headerRef}
+				style={{ background: darkMode ? '#051418' : '' }}
+			>
+				<div className='container header-container'>
+					<div className='header-left'>
+						<div className='header-name'>Pham Viet Long</div>
+					</div>
+					<div className='header-right'>
+						<ul
+							className={
+								showMenu === true
+									? 'header-menu-list'
+									: 'header-menu-list hide-menu'
+							}
+							style={{ background: darkMode ? '#051418' : '' }}
+						>
+							<li className='header-item'>
+								<a
+									href='#home'
+									className='header-link'
+									onClick={() => setShowMenu(false)}
+									style={{ color: darkMode ? 'white' : '' }}
+								>
+									Home
+								</a>
+							</li>
+							<li className='header-item'>
+								<a
+									href='#about'
+									className='header-link'
+									onClick={() => setShowMenu(false)}
+									style={{ color: darkMode ? 'white' : '' }}
+								>
+									About
+								</a>
+							</li>
+							<li className='header-item'>
+								<a
+									href='#skills'
+									className='header-link'
+									onClick={() => setShowMenu(false)}
+									style={{ color: darkMode ? 'white' : '' }}
+								>
+									Skills
+								</a>
+							</li>
+							<li className='header-item'>
+								<a
+									href='#projects'
+									className='header-link'
+									onClick={() => setShowMenu(false)}
+									style={{ color: darkMode ? 'white' : '' }}
+								>
+									Projects
+								</a>
+							</li>
+							<li className='header-item'>
+								<a
+									href='https://apps.shopify.com/facebook-multi-pixels'
+									className='header-link'
+									onClick={() => setShowMenu(false)}
+									style={{ color: darkMode ? 'white' : '' }}
+								>
+									Contact
+								</a>
+							</li>
+							<li
+								className='btn-close-menu'
+								onClick={() => setShowMenu(false)}
+								style={{ color: darkMode ? 'white' : '' }}
+							>
+								&times;
+							</li>
+						</ul>
+						<div className='btn-show-menu' onClick={() => setShowMenu(true)}>
+							<FontAwesomeIcon
+								icon={faAlignCenter}
+								className='icon-show-menu'
+								style={{ color: darkMode ? 'white' : '' }}
+							/>
+						</div>
+						<div className='toggle'>
+							<input
+								type='checkbox'
+								id='switch'
+								className='switch-input'
+								onClick={() => handleToggle()}
+							/>
+							<label
+								htmlFor='switch'
+								className='switch'
+								style={{ background: darkMode ? '#37b548' : '#102a42' }}
+							></label>
+						</div>
+					</div>
+				</div>
+			</div>
+			{/*
                 <a href='cv.pdf' download='phamvietlong cv.pdf'>download</a> 
             */}
-        </>
-    )
+		</>
+	);
 }
 
-export default Header
+export default Header;
--- src/container/hero/Hero.js
+++ src/container/hero/Hero.js
@@ -1,73 +1,110 @@
-import React from 'react'
-import './Hero.scss'
-import Typical from 'react-typical'
-import { faFacebook, faGooglePlus, faYoutube, faTwitter } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useStore } from '../../store/useStore'
+import React from 'react';
+import './Hero.scss';
+import Typical from 'react-typical';
+import {
+	faFacebook,
+	faGooglePlus,
+	faYoutube,
+	faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useStore } from '../../store/useStore';
 
-import avatar from '../../assets/avatar.jpg'
-import cv from '../../assets/cv.pdf'
+import avatar from '../../assets/avatar.jpg';
+import cv from '../../assets/cv.pdf';
 
 function Hero() {
+	const [state] = useStore();
+	const { darkMode } = state;
 
-    const [state] = useStore()
-    const { darkMode } = state
-
-    return (
-        <>
-            <div className='hero' id='home' style={{ background: darkMode ? '#051418' : '', clipPath: darkMode ? 'none' : 'ellipse(100% 100% at top)' }}>
-                <div className='container'>
-                    <div className='row'>
-                        <div className='col-lg-6 col-md-6 col-sm-12'>
-                            <div className='hero-left'>
-                                <ul className='social-list'>
-                                    <li className='social-item'>
-                                        <FontAwesomeIcon icon={faFacebook} className='social-icon social-icon-facebook' />
-                                    </li>
-                                    <li className='social-item'>
-                                        <FontAwesomeIcon icon={faGooglePlus} className='social-icon social-icon-google' />
-                                    </li>
-                                    <li className='social-item'>
-                                        <FontAwesomeIcon icon={faYoutube} className='social-icon social-icon-youtube' />
-                                    </li>
-                                    <li className='social-item'>
-                                        <FontAwesomeIcon icon={faTwitter} className='social-icon social-icon-twitter' />
-                                    </li>
-                                </ul>
-                                <div className='introduce'>
-                                    <div className='introduce-title' style={{ color: darkMode ? 'white' : '' }}>
-                                        <Typical
-                                            loop={Infinity}
-                                            steps={[
-                                                `I'm a web developer`,
-                                                1000,
-                                                `And i love beautiful girls`,
-                                                1000,
-                                            ]}
-                                            wrapper="h1"
-                                        />
-                                    </div>
-                                    <div className='introduce-desc'>
-                                        Knack of building applications with front end and back end operations
-                                    </div>
-                                </div>
-                                <div className='list-button'>
-                                    <a href='#contact' className='button'>Hire me</a>
-                                    <a href={cv} download='phamvietlong-cv.pdf' className='button'>Get CV</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='col-lg-6 col-md-6 col-sm-12'>
-                            <div className='box-avatar'>
-                                <img className='avatar' src={avatar} alt='avatar' />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-
+	return (
+		<>
+			<div
+				className='hero'
+				id='home'
+				style={{
+					background: darkMode ? '#051418' : '',
+					clipPath: darkMode ? 'none' : 'ellipse(100% 100% at top)',
+				}}
+			>
+				<div className='container'>
+					<div className='row'>
+						<div className='col-lg-6 col-md-6 col-sm-12'>
+							<div className='hero-left'>
+								<ul className='social-list'>
+									<li className='social-item'>
+										<FontAwesomeIcon
+											icon={faFacebook}
+											className='social-icon social-icon-facebook'
+										/>
+									</li>
+									<li className='social-item'>
+										<FontAwesomeIcon
+											icon={faGooglePlus}
+											className='social-icon social-icon-google'
+										/>
+									</li>
+									<li className='social-item'>
+										<FontAwesomeIcon
+											icon={faYoutube}
+											className='social-icon social-icon-youtube'
+										/>
+									</li>
+									<li className='social-item'>
+										<FontAwesomeIcon
+											icon={faTwitter}
+											className='social-icon social-icon-twitter'
+										/>
+									</li>
+								</ul>
+								<div className='introduce'>
+									<div
+										className='introduce-title'
+										style={{ color: darkMode ? 'white' : '' }}
+									>
+										<Typical
+											loop={Infinity}
+											steps={[
+												`I'm a web developer`,
+												1000,
+												`And i love beautiful girls`,
+												1000,
+											]}
+											wrapper='h1'
+										/>
+									</div>
+									<div className='introduce-desc'>
+										Knack of building applications with front end and back end
+										operations
+									</div>
+								</div>
+								<div className='list-button'>
+									<a
+										href='https://apps.shopify.com/facebook-multi-pixels'
+										className='button'
+									>
+										Hire me
+									</a>
+									<a
+										href={cv}
+										download='phamvietlong-cv.pdf'
+										className='button'
+									>
+										Get CV
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className='col-lg-6 col-md-6 col-sm-12'>
+							<div className='box-avatar'>
+								<img className='avatar' src={avatar} alt='avatar' />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default Hero
+export default Hero;
